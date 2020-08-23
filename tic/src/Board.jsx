17ecc0a6090b8dc6d463b8ca5f6cd7460673b221
Yaroslav <dombrovskiy91@gmail.com>
2020-08-23 14:27:37 +0300

    method handeClick(i) in Board Comp

--- tic/src/Board.jsx
+++ tic/src/Board.jsx
@@ -5,6 +5,13 @@ export default class Board extends React.Component {
     state = {
         squares : Array(9).fill(null)
     };
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({
+            squares: squares
+        });
+    }
     renderSquare(i) {
         return <Square
             value={this.state.squares[i]}
